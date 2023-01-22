8597ccfaa231be2589ae279169d0d0f47f6c8698
Anandhu <anandhu7299@gmail.com>
2023-01-23 00:19:27 +0530

    Skeltom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { config } from './config';
+
 
 function App() {
+
+
+  const [items, setItems] = useState(config.data)
+  const [grandTotal, setGrandTotal] = useState(0)
+
+
+  const handleInputChange = (e, index) => {
+    let arr = [...items]
+    console.log(e.target.value);
+    arr[index].qty = e.target.value
+    arr[index].sum = Number(e.target.value) * arr[index].price
+    setItems(arr)
+  }
+
+  const handleMinus = (index) => {
+    let arr = [...items]
+    arr[index].qty -= 1
+    if (Number(arr[index].qty) <= 0) {
+      arr[index].qty = 0 
+    }
+    arr[index].sum = Number(arr[index].qty) * arr[index].price
+    setItems(arr)
+  }
+
+  const handlePlus = (index) => {
+    let arr = [...items]
+    arr[index].qty += 1
+    arr[index].sum = Number(arr[index].qty) * arr[index].price
+    setItems(arr)
+  }
+
+
+  useEffect(() => {
+    let sum = 0
+    items.forEach((item) => {
+      sum += item.sum
+    })
+    setGrandTotal(sum)
+  }, [items])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ALDA Pay</h1>
+
+      <table>
+        <thead>
+          <th>Item</th>
+          <th>Price</th>
+          <th>Qty</th>
+          <th>Total</th>
+        </thead>
+        <tbody>
+          {items.map((item, index) =>
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>
+                <button onClick={()=>handleMinus(index)}> -</button>
+                <input
+                  type="number"
+                  min={0}
+                  max={99}
+                  value={item.qty}
+                  onChange={(e) => handleInputChange(e, index)}
+                />
+                <button onClick={()=>handlePlus(index)}> +</button>
+
+              </td>
+              <td>{item.sum}</td>
+            </tr>
+          )}
+
+        </tbody>
+      </table>
+      <div>
+        Grand Total  = {grandTotal}
+      </div>
+      <a href={`upi://pay?pa=${config.upiId}&pn=${config.name}&cu=INR&am=${grandTotal}`} >Pay Now !</a>
+
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,31 @@
+export const config = {
+    upiId: process.env.UPIID,
+    name: process.env.NAME,
+    data : [
+        {
+            id: 1,
+            name: 'Snacks ( Pazhampori, uzhunnu vada, paripu vada, ...',
+            price: 10,
+            qty: 0,
+            sum: 0
+        },
+        {
+            id: 2,
+            name: 'Drinks (leamon Tea, Geera Soda).',
+            price: 10,
+            qty: 0,
+            sum: 0
+    
+        },
+        {
+            id: 3,
+            name: 'Egg puffs.',
+            price: 17,
+            qty: 0,
+            sum: 0
+        }
+    
+    ]
+}
+
+
